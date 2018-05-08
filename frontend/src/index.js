6b441ac72c00aee3adb72a74f2bbd38e1fa18f75
mmcneeley <mmcneeley@gmail.com>
2018-05-08 16:50:03 -0400

    added a yml and other things to hide keys

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-})
+  let showsUl = document.getElementById('shows')
+
+  fetch('http://localhost:3000/api/v1/shows')
+    .then(response => response.json())
+    .then(shows => {
+
+      console.log("shows json response", shows);
+      shows.forEach(show => {
+        let showLi = document.createElement('li')
+        showLi.innerHTML = `${show["artist"]["name"]} at ${show["venue"]["name"]}`
+        showsUl.append(showLi)
+      })
+    }) // end of fetch
+
+}) // end of dom content loaded
