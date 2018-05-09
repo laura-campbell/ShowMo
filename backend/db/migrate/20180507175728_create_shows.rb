cec017df241a80a2381ae6e1cbff12e223e6d4ee
mmcneeley <mmcneeley@gmail.com>
2018-05-09 18:12:50 -0400

    switch the backend

--- backend/db/migrate/20180507175728_create_shows.rb
+++ backend/db/migrate/20180507175728_create_shows.rb
@@ -3,8 +3,12 @@ class CreateShows < ActiveRecord::Migration[5.1]
     create_table :shows do |t|
       t.references :artist, foreign_key: true
       t.references :venue, foreign_key: true
+      t.string :date
+      t.string :time
       t.datetime :showtime
-      t.integer :api_show_id
+      t.integer :songkick_id
+      t.float :songkick_popularity
+      t.text :songkick_url
 
       t.timestamps
     end
